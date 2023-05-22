977d1d8e1002eeafdfadf58f1c1b8196971c62c8
Pamela UMWALI <p.umwali@alustudent.com>
2023-05-22 18:12:45 +0200

    filestorage class

---
+++ models/engine/file.storage.py
@@ -0,0 +1,32 @@
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as file:
+                serialized_objects = json.load(file)
+                for key, value in serialized_objects.items():
+                    class_name, obj_id = key.split('.')
+                    class_obj = eval(class_name)
+                    obj = class_obj(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
