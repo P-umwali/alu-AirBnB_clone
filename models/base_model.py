7a0b865d3e69df10f68472bb672614bc421caf96
Pamela UMWALI <p.umwali@alustudent.com>
2023-05-23 21:42:20 +0200

    hoping it works

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -31,3 +32,17 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+    def save(self):
+        storage.new(self)
+        storage.save()
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            self.__set_attributes(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
+
+    # Rest of the BaseModel class implementation...
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,72 +1,36 @@
-#!/usr/bin/python3
-"""This module is responsible for managing the serialization,
-and deserialization of objects to and from a JSON file."""
-
 import json
-import os
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id."""
-        key = obj.__class__.__name__ + '.' + obj.id
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
     def save(self):
-        """serializez __objects to the JSON file (path: __file_path)."""
-        data = {}
-        for key, value in self.__objects.items():
-            if hasattr(value, "to_dict"):
-                data[key] = value.to_dict()
-            else:
-                data[key] = value.__dict__()
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
 
         with open(self.__file_path, 'w') as file:
-            json.dump(data, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
-        """deserializes the JSON file to __objects."""
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    class_name, obj_id = key.split(".")
-                    if class_name == "BaseModel":
-                        from models.base_model import BaseModel
-                        cls = BaseModel
-
-                    elif class_name == "User":
-                        cls = User
-
-                    elif class_name == "Place":
-                        cls = Place
-
-                    elif class_name == "State":
-                        cls = State
-
-                    elif class_name == "City":
-                        cls = City
-
-                    elif class_name == "Amenity":
-                        cls = Amenity
-
-                    elif class_name == "Review":
-                        cls = Review
-
-                    else:
-                        cls = globals()[class_name]
-                    obj = cls(**value)
-                    self.__objects[key] = obj
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, obj_dict in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    obj_class = models.classes[class_name]
+                    obj_instance = obj_class(**obj_dict)
+                    self.__objects[key] = obj_instance
+        except FileNotFoundError:
+            pass
