6290121ef63d044bdd735a94e9fd762ebd6fb16d
Nelson Himbaza <Nelsonhimbaza@gmail.com>
2023-05-26 19:47:13 +0200

    Modified base model file

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""
-    Define 'BaseModel' class
-"""
+"""Define 'BaseModel' class"""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """
-        Represent 'BaseModel' class
-    """
+    """Represent 'BaseModel' class"""
 
     def __init__(self, *args, **kwargs):
-        """
-            Initialize new 'BaseModel' instance
-            Arguments:
-                *args (any): unused
-                **kwargs (dict): key/value pairs of attributes
-        """
+        """Initialize new 'BaseModel' instanceArguments:*args (any): unused**kwargs (dict): key/value pairs of attributes"""
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
@@ -33,16 +24,12 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """
-            Save the model instance to the storage engine
-        """
+        """Save the model instance to the storage engine"""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """
-            Return dictionary representation of the model instance
-        """
+        """Return dictionary representation of the model instance"""
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
@@ -50,9 +37,6 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """
-            Return string representation of the model instance
-        """
+        """Return string representation of the model instance"""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
-    
